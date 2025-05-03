2711b8198564ee05d0aecddb92a04b1ae088f5ce
kalamgit143 <magnitiait@gmail.com>
2025-05-03 09:42:08 +0530

    My next commit

--- src/test/java/playwrighttests/PlaywrightTest1.java
+++ src/test/java/playwrighttests/PlaywrightTest1.java
@@ -15,15 +15,16 @@ public class PlaywrightTest1
         Browser browser=pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         BrowserContext context=browser.newContext();
         Page driver=context.newPage();
-        // Navigate to the site
+        // Navigate to the site using playwright code
         driver.navigate("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test");
-        // Wait for iframe and switch to it
+        // Wait for iframe and switch to it using playwright code
         driver.frameLocator("#iframeResult").locator("input[name='fname']").fill("abdul");
         driver.frameLocator("#iframeResult").locator("#lname").fill("kalam");
-        // Click the submit button
+        // Click the submit button using playwright code
         driver.frameLocator("#iframeResult").locator("//input[@value='Submit']").click();
-        // Wait for a while to observe the result
+        // Wait for a while to observe the result using playwright code
         driver.waitForTimeout(5000);
+        //Close site
 	}
 
 }
