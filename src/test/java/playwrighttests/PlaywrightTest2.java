44b75515611c53d71bd59e6548b618dce0765eaf
kalamgit143 <magnitiait@gmail.com>
2025-05-04 09:51:13 +0530

    sunday modification

--- src/test/java/playwrighttests/PlaywrightTest2.java
+++ src/test/java/playwrighttests/PlaywrightTest2.java
@@ -1,10 +1,22 @@
 package playwrighttests;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 public class PlaywrightTest2 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception
 	{
-		//
-		System.out.println("Hi");
+		//Open firefox browser
+		try (Playwright playwright = Playwright.create()) {
+            BrowserType firefox = playwright.firefox();
+            Browser browser = firefox.launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://www.irctc.co.in");
+            Thread.sleep(5000);
+            System.out.println("IRCTC site launched in Firefox browser.");
+        }
 	}
 }
